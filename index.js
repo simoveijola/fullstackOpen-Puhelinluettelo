0a24fe7963987a0d4aa509c4fc6ee13671e438ca
Simo Veijola <veijols2@aalto.fi>
2023-11-15 13:56:06 +0200

    uibuild

--- index.js
+++ index.js
@@ -42,20 +42,7 @@ app.get('/api/persons', (request, response) => {
         response.json(persons)
     })
 })
-/*
-app.get('/info', (request, response) => {
-    const date = new Date()
-    response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
-    response.write(`${date}`)
-    response.send()
-})
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
-*/
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
@@ -83,8 +70,9 @@ app.post('/api/persons', (request, response) => {
             name: body.name,
             number: body.number,
         })
-        persons = persons.concat(person)
-        response.json(person)
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        })
     }
 })
 
