ce342d9c035d46802c1a2ed1208e8af1486acf5d
Simo Veijola <veijols2@aalto.fi>
2023-11-17 18:41:20 +0200

    Add exercises ... to 3.22

--- models/person.js
+++ models/person.js
@@ -1,43 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+console.log("connecting to", url)
 
-mongoose.connect(url).then(result => {
-    console.log('connected to the database')
+mongoose.connect(url).then(() => {
+  console.log("connected to the database")
 }).catch(error => {
-    console.log(`error connecting to the database: ${error.message}`)
+  console.log(`error connecting to the database: ${error.message}`)
 })
 
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
+  id: Number,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{1,}$/.test(v)
+      },
+      message: "Incorrect number format. Accepted formats: xx-yyyyyy... and xxx-yyyyy..."
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function (v) {
-                return /^\d{2,3}-\d{1,}$/.test(v)
-            },
-            message: "Incorrect number format. Accepted formats: xx-yyyyyy... and xxx-yyyyy..."
-        },
-        required: true,
-    },
+    required: true,
+  },
 })
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema)
--- mongo.js
+++ mongo.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const argv = process.argv
 
 if (argv.length < 3) {
-    console.log("please give password to the database")
-    process.exit(1)
-} else if (argv.length == 4) {
-    console.log("please provide either only the password of the database to read the contents \nor the password with both the name and the number of the person to be added to the database")
-    process.exit(1)
+  console.log("please give password to the database")
+  process.exit(1)
+} else if (argv.length === 4) {
+  console.log("please provide either only the password of the database to read the contents \nor the password with both the name and the number of the person to be added to the database")
+  process.exit(1)
 } else if (argv.length > 5) {
-    console.log("too many arguments")
-    process.exit(1)
+  console.log("too many arguments")
+  process.exit(1)
 }
 
 const password = argv[2]
 const url = `mongodb+srv://simoveijola:${password}@cluster0.fpqthvb.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model("Person", personSchema)
 
 if (argv.length === 3) {
-    console.log('phonebook:')
-    Person.find({}).then(persons => {
-        persons.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log("phonebook:")
+  Person.find({}).then(persons => {
+    persons.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 } else {
-    // const id = Math.floor(Math.random() * 1000000)
-    const newPerson = new Person({
-        name: argv[3],
-        number: argv[4],
-    })
-    newPerson.save().then(result => {
-        console.log(`added ${newPerson.name} number ${newPerson.number} to the phonebook`)
-        mongoose.connection.close()
-    })
+  // const id = Math.floor(Math.random() * 1000000)
+  const newPerson = new Person({
+    name: argv[3],
+    number: argv[4],
+  })
+  newPerson.save().then(() => {
+    console.log(`added ${newPerson.name} number ${newPerson.number} to the phonebook`)
+    mongoose.connection.close()
+  })
 }
 
